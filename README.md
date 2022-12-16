6de9b2babab435706d7a88db2867d5c7572dbfab
eliadbz <eliadbz@gmail.com>
2022-12-16 17:38:40 +0200

    Added fixed status code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Enum Subdomain by Status Code
+
+This tool enumerates subdomains using status codes return by the HTTP status codes. This makes it useful for enumerating web servers that use engines like NGINX, whose subdomains can be enumerated by the status code returned.
+
+
+Requirements
+---
+- golang 1.19+
+- make
+
+
+
+Installation
+--
+`sudo make install`
+
+
+Usage
+--
+To get all the parameters available, use: `esdsc -h`
+
+example: `esdsc -w path/to/wordlist -ssl -s 301 -d example.com`
--- util/flags.go
+++ util/flags.go
@@ -14,7 +14,7 @@ func ParseFlags() map[string]interface{}{
 
 	var wordlistPath *string = flag.String("w", "", "Subdomain names wordlist") //mandatory
 	var baseDomain *string = flag.String("d", "", "The base domain") //mandatory
-	var statusCode *int = flag.Int("s", 304, "Status code for indicating a non-existent subdomain")
+	var statusCode *int = flag.Int("s", 301, "Status code for indicating a non-existent subdomain")
 	var isSSL *bool = flag.Bool("ssl", false, "Indicate whether it's SSL or plaintext url")
 	flag.Parse()
 
